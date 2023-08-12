3e034260ac693a171fe430c7b479f43c261e352f
Roman G <roma.gubin2011@yandex.ru>
2023-08-12 07:55:14 +0500

    functioons home work4

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,6 +1,6 @@
 function getArrayParams(...arr) {
-  
-  let min = arr[0];
+
+	let min = arr[0];
 	let max = arr[0];
 	let sum = 0;
 
@@ -10,7 +10,7 @@ function getArrayParams(...arr) {
 		} else if (arr[i] < min) {
 			min = arr[i];
 		}
-    sum = sum + arr[i];
+		sum = sum + arr[i];
 	}
 	avg = +(sum / arr.length).toFixed(2);
 	return {
@@ -21,79 +21,79 @@ function getArrayParams(...arr) {
 }
 
 function summElementsWorker(...arr) {
-  let sum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    sum = arr[i] + sum;
-  }
-  return sum;
+	let sum = 0;
+	for (let i = 0; i < arr.length; i++) {
+		sum = arr[i] + sum;
+	}
+	return sum;
 }
 
 function differenceMaxMinWorker(...arr) {
 	if (arr.length === 0) {
-    return 0;
-  }
+		return 0;
+	}
 
-  let min = arr[0];
+	let min = arr[0];
 	let max = arr[0];
 
 	for (let i = 1; i < arr.length; i++) {
 		if (arr.length === 0) {
-      return {
-        diff: 0,
-      };
-    } else if (arr[i] > max) {
+			return {
+				diff: 0,
+			};
+		} else if (arr[i] > max) {
 			max = arr[i];
 		} else if (arr[i] < min) {
 			min = arr[i];
-		} 
+		}
 	}
 	let diff = max - min;
 	return diff;
 }
 
 function differenceEvenOddWorker(...arr) {
-  let sumEvenElement = 0; //сум чет
-  let sumOddElement = 0; //сум неч
+	let sumEvenElement = 0; //сум чет
+	let sumOddElement = 0; //сум неч
 
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] % 2 == 0) {
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] % 2 == 0) {
 			sumEvenElement += arr[i];
 		} else {
 			sumOddElement += arr[i];
-		} 
+		}
 	}
-  let diff = sumEvenElement - sumOddElement;
+	let diff = sumEvenElement - sumOddElement;
 	return diff;
 }
 
 function averageEvenElementsWorker(...arr) {
-  let sumEvenElement = 0; //сум чет
-  let countEvenElementt = 0; //кол-во четн
+	let sumEvenElement = 0; //сум чет
+	let countEvenElementt = 0; //кол-во четн
 
-  if (arr.length === 0) {
-    return 0;
-  }
+	if (arr.length === 0) {
+		return 0;
+	}
 
 	for (let i = 0; i < arr.length; i++) {
-    if (arr[i] % 2 == 0) {
+		if (arr[i] % 2 == 0) {
 			sumEvenElement += arr[i];
 			countEvenElementt += 1;
-		} 
+		}
 	}
 	let avg = +(sumEvenElement / countEvenElementt);
 	return avg;
 }
 
-function makeWork (arrOfArr, func) {
-  let maxWorkerResult = -Infinity;
+function makeWork(arrOfArr, func) {
+	let maxWorkerResult = -Infinity;
 
-  for (let i = 0; i < arrOfArr.length; i++) {
-		const max = func (...arrOfArr[i]);
+	for (let i = 0; i < arrOfArr.length; i++) {
+		const max = func(...arrOfArr[i]);
 
-    if (max > maxWorkerResult) {
-      maxWorkerResult = max;
-    }
+		if (max > maxWorkerResult) {
+			maxWorkerResult = max;
+		}
 	}
 
-  return maxWorkerResult;
+	return maxWorkerResult;
 }
