0bf7e7d22b3f682a6bfaa77bee341ffdcb9a863b
Roman G <roma.gubin2011@yandex.ru>
2023-08-14 21:55:43 +0500

    functioons home work objects2

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -10,26 +10,33 @@ Student.prototype.setSubject = function(subjectName) {
 }
 
 Student.prototype.addMarks = function(...marks) {
-	if (this.marks.length >= 0) {
-		this.marks.push(...marks);
-	}
+	if (this.marks === undefined) {
+		return;
+	} else {
+    this.marks.push(...marks);
+  }
 }
 
 Student.prototype.getAverage = function() {
-	if (this.marks.length === 0) {
+	if (this.marks.length === 0 || this.marks === undefined) {
 		return 0;
-	} else {
+	}
 		let sum = 0;
 		for (let i = 0; i < this.marks.length; i++) {
 			sum = sum + this.marks[i];
 		};
 		return sum / this.marks.length;
-	}
 }
 
 Student.prototype.exclude = function(reason) {
 	delete this.subject;
 	delete this.marks;
-	delete this.excluded;
 	this.excluded = reason;
 }
+
+let student1 = new Student("Василиса", "женский", 19);
+student1.setSubject("Algebra");
+console.log(student1.getAverage()); // 0
+student1.addMarks(4, 5, 4, 5);
+console.log(student1.getAverage()); // 4.5
+console.log(student1);
